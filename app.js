58726614f1caa6ad42db4d812d0ded3b6d40e3c4
AndreyMurashov <amourashov@yandex.ru>
2022-10-14 21:34:21 +0300

    дипломная работа бэкенд начало

---
+++ app.js
@@ -0,0 +1,77 @@
+// require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+// const { celebrate, Joi, errors } = require('celebrate');
+// const auth = require('./middlewares/auth');
+// const userRouter = require('./routes/users');
+// const { login, createUser } = require('./controllers/users');
+// const cardRouter = require('./routes/cards');
+// const DefaultError = require('./errors/DefaultError');
+// const NotFoundError = require('./errors/NotFoundError');
+// const cors = require('./midlewares/cors');
+// const { requestLogger, errorLogger } = require('./midlewares/logger');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+// const absentisPage = (req, res, next) => {
+//   next(new NotFoundError('Страница не найдена'));
+// };
+
+app.use(express.json());
+// app.use(cors);
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+// app.use(requestLogger);
+
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string()
+//     .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//   }),
+// }), createUser);
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//   }),
+// }), login);
+// app.use(auth);
+// app.use(userRouter);
+// app.use(cardRouter);
+// app.all('*', absentisPage);
+
+// app.use(errorLogger);
+
+// app.use(errors());
+
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message } = err;
+//   res.status(statusCode).send({ message: statusCode === 500 ?
+// 'На сервере произошла ошибка' : message });
+// });
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb', {
+    useNewUrlParser: true,
+  })
+  .then(console.log('DB OK'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Произошла ошибка при запуске сервера');
+  }
+  console.log(`Сервер запущен на ${PORT} порту`);
+});
---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const LoginError = require('../errors/LoginError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    next(new LoginError('Необходима авторизация'));
+  }
+  const token = authorization.replace(/Bearer\s?/, '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'}`);
+  } catch (err) {
+    next(new LoginError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+  next();
+};
