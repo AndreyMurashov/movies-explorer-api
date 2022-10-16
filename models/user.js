02c48818059e81ac98c85b4d0e01a9bdc9f192a6
AndreyMurashov <amourashov@yandex.ru>
2022-10-16 22:47:41 +0300

    дипломная работа промежуточное сохранение

--- models/user.js
+++ models/user.js
@@ -1,3 +1,44 @@
-email — почта пользователя, по которой он регистрируется. Это обязательное поле, уникальное для каждого пользователя. Также оно должно валидироваться на соответствие схеме электронной почты.
-password — хеш пароля. Обязательное поле-строка. Нужно задать поведение по умолчанию, чтобы база данных не возвращала это поле.
-name — имя пользователя, например: Александр или Мария. Это обязательное поле-строка от 2 до 30 символов.
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Проверьте адрес почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
